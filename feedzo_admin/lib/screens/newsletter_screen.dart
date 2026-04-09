7d0a5e348700ceaf1470814f096ff75273eab423
masthan008 <masthanvallibaba009@gmail.com>
2026-04-09 21:08:51 +0530

    Fix DateFormat import in newsletter_screen.dart

--- feedzo_admin/lib/screens/newsletter_screen.dart
+++ feedzo_admin/lib/screens/newsletter_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:intl/intl.dart';
 import '../core/theme.dart';
 import '../models/newsletter_model.dart';
 import '../services/newsletter_service.dart';
